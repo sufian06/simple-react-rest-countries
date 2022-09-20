67a97662b1c829523f5f858be6d36902450a7932
sufian <sufian4se@gmail.com>
2022-09-20 22:34:28 +0600

    create child component and pass data via props

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Country.css';
 
 const Country = (props) => {
     return (
-        <div>
+        <div className='country'>
             <h2>Country Name: {props.name}</h2>
+            <p>Population: {props.population}</p>
+            <p><small>Area: {props.area}</small></p>
         </div>
     );
 };
